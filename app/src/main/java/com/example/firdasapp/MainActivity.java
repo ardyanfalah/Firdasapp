b4f95d6bb5fdde2bd75961b018ffac0edfa42b68
Ardyan Falah <ardyan.falah@sangkuriang.co.id>
2019-10-07 11:43:04 +0700

    flash

--- app/src/main/java/com/example/firdasapp/MainActivity.java
+++ app/src/main/java/com/example/firdasapp/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.firdasapp;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        turnOnFlash();
+        turnOFfFlash();
+    }
+    public void turnOnFlash(){
+        Button btn = findViewById(R.id.btn_flash);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View view) {
+                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+
+                try {
+                    String cameraId = cameraManager.getCameraIdList()[0];
+                    cameraManager.setTorchMode(cameraId, true);
+                } catch (CameraAccessException e) {
+                }
+            }
+        });
+    }
+
+    public void turnOFfFlash(){
+        Button btnOff = findViewById(R.id.btn_flash_off);
+        btnOff.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.M)
+            @Override
+            public void onClick(View v) {
+                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+                try {
+                    {
+                        String cameraId = cameraManager.getCameraIdList()[0];
+                        cameraManager.setTorchMode(cameraId,false);
+                    }
+                } catch (CameraAccessException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
