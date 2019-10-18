67b4f3df94ff76cf4fc9c6e05f2990e75e0112c6
Ardyan Falah <ardyan.falah@sangkuriang.co.id>
2019-10-18 19:17:13 +0700

    alarm flash vibrate

---
+++ app/src/main/java/com/example/firdasapp/AlarmReceiver.java
@@ -0,0 +1,58 @@
+package com.example.firdasapp;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.widget.Toast;
+
+import androidx.annotation.RequiresApi;
+import androidx.legacy.content.WakefulBroadcastReceiver;
+
+public class AlarmReceiver extends WakefulBroadcastReceiver
+{
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        //this will update the UI with message
+        MainActivity inst = MainActivity.instance();
+        inst.setAlarmText("Alarm! Wake up! Wake up!");
+        CameraManager cameraManager = (CameraManager) inst.getSystemService(Context.CAMERA_SERVICE);
+        String cameraId = null;
+        try {
+            cameraId = cameraManager.getCameraIdList()[0];
+            cameraManager.setTorchMode(cameraId, true);
+        } catch (CameraAccessException e) {
+            e.printStackTrace();
+        }
+        Vibrator v = (Vibrator) inst.getSystemService(Context.VIBRATOR_SERVICE);
+
+        long[] pattern = {0, 1000, 1000};
+
+        v.vibrate(pattern, 0);
+        //this will sound the alarm tone
+        //this will sound the alarm once, if you wish to
+        //raise alarm in loop continuously then use MediaPlayer and setLooping(true)
+        Uri alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+        if (alarmUri == null) {
+            alarmUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        }
+        Ringtone ringtone = RingtoneManager.getRingtone(context, alarmUri);
+        ringtone.play();
+
+        //this will send a notification message
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                AlarmService.class.getName());
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
+    }
+}
--- app/src/main/java/com/example/firdasapp/MainActivity.java
+++ app/src/main/java/com/example/firdasapp/MainActivity.java
@@ -3,48 +3,75 @@ package com.example.firdasapp;
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
+import android.widget.ToggleButton;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
+
+    AlarmManager alarmManager;
+    private PendingIntent pendingIntent;
+    private TimePicker alarmTimePicker;
+    private static MainActivity inst;
+    private TextView alarmTextView;
+
+    public static MainActivity instance() {
+        return inst;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        inst = this;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        turnOnFlash();
-        turnOFfFlash();
-    }
-    public void turnOnFlash(){
-        Button btn = findViewById(R.id.btn_flash);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void onClick(View view) {
-                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
-
-                try {
-                    String cameraId = cameraManager.getCameraIdList()[0];
-                    cameraManager.setTorchMode(cameraId, true);
-                } catch (CameraAccessException e) {
-                }
-            }
-        });
+        alarmTimePicker = (TimePicker) findViewById(R.id.alarmTimePicker);
+        alarmTextView = (TextView) findViewById(R.id.alarmText);
+        ToggleButton alarmToggle = (ToggleButton) findViewById(R.id.alarmToggle);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
     }
 
-    public void turnOFfFlash(){
-        Button btnOff = findViewById(R.id.btn_flash_off);
-        btnOff.setOnClickListener(new View.OnClickListener() {
-            @RequiresApi(api = Build.VERSION_CODES.M)
-            @Override
-            public void onClick(View v) {
-                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void OnToggleClicked(View view) {
+        if (((ToggleButton) view).isChecked()) {
+            Log.d("MyActivity", "Alarm On");
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, alarmTimePicker.getCurrentHour());
+            calendar.set(Calendar.MINUTE, alarmTimePicker.getCurrentMinute());
+            Intent myIntent = new Intent(MainActivity.this, AlarmReceiver.class);
+            pendingIntent = PendingIntent.getBroadcast(MainActivity.this, 0, myIntent, 0);
+            alarmManager.set(AlarmManager.RTC, calendar.getTimeInMillis(), pendingIntent);
+
+        } else {
+            alarmManager.cancel(pendingIntent);
+            setAlarmText("");
+            Log.d("MyActivity", "Alarm Off");
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void setAlarmText(String alarmText) {
+        alarmTextView.setText(alarmText);
+        CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
                 try {
                     {
                         String cameraId = cameraManager.getCameraIdList()[0];
@@ -53,7 +80,43 @@ public class MainActivity extends AppCompatActivity {
                 } catch (CameraAccessException e) {
                     e.printStackTrace();
                 }
-            }
-        });
+        Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        v.cancel();
+    }
+
+    public void turnOnFlash(){
+////        Button btn = findViewById(R.id.btn_flash);
+////        btn.setOnClickListener(new View.OnClickListener() {
+////            @RequiresApi(api = Build.VERSION_CODES.M)
+////            @Override
+////            public void onClick(View view) {
+//                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+////
+////                try {
+//                    String cameraId = cameraManager.getCameraIdList()[0];
+//                    cameraManager.setTorchMode(cameraId, true);
+////                } catch (CameraAccessException e) {
+////                }
+////            }
+////        });
+    }
+
+    public void turnOFfFlash(){
+//        Button btnOff = findViewById(R.id.btn_flash_off);
+//        btnOff.setOnClickListener(new View.OnClickListener() {
+//            @RequiresApi(api = Build.VERSION_CODES.M)
+//            @Override
+//            public void onClick(View v) {
+//                CameraManager cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+//                try {
+//                    {
+//                        String cameraId = cameraManager.getCameraIdList()[0];
+//                        cameraManager.setTorchMode(cameraId,false);
+//                    }
+//                } catch (CameraAccessException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        });
     }
 }
